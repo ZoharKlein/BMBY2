6158454a7f5ce28bcb064d0d45538c3fe50264e7
ZoharKlein <you@example.com>
2019-07-21 18:21:29 +0300

    same with bug fix

--- controllers/dashboard/dashboard.js
+++ controllers/dashboard/dashboard.js
@@ -3,25 +3,23 @@ const payLaneMenuForCustomer  = [
   {timeLength: 1,
   price: 49.99,
   coin: '$',
-  details: ["1. bla bla bla", "2. bla bla bla","3. bla bla bla"]
+  details: ["bla bla bla", "bla bla bla","bla bla bla"]
   },
   {timeLength: 2,
       price: 89.99,
       coin: '$',
-      details: ["1. bla bla bla", "2. bla bla bla","3. bla bla bla"]
+      details: ["bla bla bla", "bla bla bla","bla bla bla"]
       },
   {timeLength: 3,
       price: 129.99,
       coin: '$',
-      details: ["1. bla bla bla", "2. bla bla bla","3. bla bla bla"]
+      details: ["bla bla bla", "bla bla bla","bla bla bla"]
   }
 ]
 
 
 exports.getDashboard = (req, res, next) => {
   
-    console.log(payLaneMenuForCustomer)
-
       res.render('dashboard/dashboard',{
         title: "BMBY2 Dashboard",
         customer: global.loginCustomer,
